f4ead3138c3ac5f5d7e89a94b0e4a6df05670ac5
unknown <user@mil8a-118a-11.mmcs.sfedu.ru>
2018-09-11 16:28:18 +0300

    added buttons and text field
    Committer: mrRomanovsky <rmansorokin@gmail.com>

--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -16,5 +16,15 @@ namespace Lab2
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
