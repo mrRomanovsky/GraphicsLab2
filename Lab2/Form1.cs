0a94a0a4344e27a4c9a0b3b9cebfbe210913382c
iambeardier <iambeardier@yandex.com>
2018-09-12 16:09:49 +0300

    add task3 form, commented task1 code

--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -12,7 +12,7 @@ namespace Lab2
 {
 	public partial class Form1 : Form
 	{
-		Task1EqualGrey task1EqualGrey;
+		//Task1EqualGrey task1EqualGrey;
 
 		public Form1()
 		{
@@ -21,7 +21,7 @@ namespace Lab2
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			Bitmap image = (Bitmap) Bitmap.FromFile(textBox1.Text);
+		/*	Bitmap image = (Bitmap) Bitmap.FromFile(textBox1.Text);
 			for (int i = 0; i < image.Width; ++i)
 				for (int j = 0; j < image.Height; ++j) {
 					var pixel = image.GetPixel(i, j);
@@ -30,12 +30,18 @@ namespace Lab2
 				}
 			task1EqualGrey = new Task1EqualGrey();
 			task1EqualGrey.ShowImage(image);
-			task1EqualGrey.ShowDialog();
+			task1EqualGrey.ShowDialog();*/
 		}
 
 		private void label1_Click(object sender, EventArgs e)
 		{
 
 		}
-	}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task3 task3 = new Task3();
+            task3.ShowDialog();
+        }
+    }
 }
