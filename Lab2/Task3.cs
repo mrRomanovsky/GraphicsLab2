f3978529e8a0071ca845db3a66a86a5216914510
iambeardier <iambeardier@yandex.com>
2018-09-14 11:13:27 +0300

    task3 almost done

--- Lab2/Task3.cs
+++ Lab2/Task3.cs
@@ -12,7 +12,7 @@ namespace Lab2
 {
     public partial class Task3 : Form
     {
-
+        private float EPS = 0.0001f;
         private Bitmap img;
         public Task3()
         {
@@ -26,34 +26,83 @@ namespace Lab2
                 for (int j = 0; j < image.Height; ++j)
                 {
                     var pixel = image.GetPixel(i, j);
-                    maxRGB = Math.Max(pixel.R / 255f, Math.Max(pixel.G / 255f, Math.Max(pixel.B / 255f, maxRGB)));
-                    minRGB = Math.Min(pixel.R / 255f, Math.Min(pixel.G / 255f, Math.Min(pixel.B / 255f, minRGB)));
+                    float pixelR = pixel.R / 255f;
+                    float pixelG = pixel.G / 255f;
+                    float pixelB = pixel.B / 255f;
+                    maxRGB = Math.Max(pixelR, Math.Max(pixelG, Math.Max(pixelB, maxRGB)));
+                    minRGB = Math.Min(pixelR, Math.Min(pixelG, Math.Min(pixelB, minRGB)));
                     float delta = maxRGB - minRGB;
-                    float H;
-                    float S;
+                    //RGB -> HSV
+                    float H = 0;
+                    float S = 0;
                     float V = maxRGB;
                     if (maxRGB == 0)
                         S = 0;
                     else
                         S = 1 - minRGB / maxRGB;
-                    if (maxRGB == minRGB)
+                    if (maxRGB - minRGB < EPS)
                         H = 0;
                     else
                     {
-                        if(maxRGB == pixel.R / 255f && pixel.G / 255f >= pixel.B / 255f)
-                            H = 60 * (pixel.G / 255f - pixel.B / 255f) / (maxRGB - minRGB);
-                        else if(maxRGB == pixel.R / 255f && pixel.G / 255f < pixel.B / 255f)
-                            H = 60 * (pixel.G / 255f - pixel.B / 255f) / (maxRGB - minRGB) + 360;
-                        else if(maxRGB == pixel.G / 255f)
-                            H = 60 * (pixel.B / 255f - pixel.R / 255f) / (maxRGB - minRGB) + 120;
-                        else if (maxRGB == pixel.B / 255f)
-                            H = 60 * (pixel.R / 255f - pixel.G / 255f) / (maxRGB - minRGB) + 240;
+                        if((maxRGB - pixelR) < EPS && pixelG >= pixelB)
+                            H = 60 * (pixelG - pixelB) / (maxRGB - minRGB);
+                        else if((maxRGB - pixelR) < EPS && pixelG < pixelB)
+                            H = 60 * (pixelG - pixelB) / (maxRGB - minRGB) + 360;
+                        else if((maxRGB - pixelG) < EPS)
+                            H = 60 * (pixelB - pixelR) / (maxRGB - minRGB) + 120;
+                        else if ((maxRGB - pixelB) < EPS)
+                            H = 60 * (pixelR - pixelG) / (maxRGB - minRGB) + 240;
                     }
+                    //HSV -> RGB
+                    S *= 100;
+                    V *= 100;
+                    int Hi = (int)(Math.Floor(H / 60) % 6);
+                    var Vmin = (100 - S) * V / 100;
+                    var a = (V - Vmin) * (H % 60) / 60;
+                    var Vinc = Vmin + a;
+                    var Vdec = V - a;
+                    float R = 0, G = 0, B = 0;
+                    switch (Hi)
+                    {
+                        case 0:
+                            R = V;
+                            G = Vinc;
+                            B = Vmin;
+                            break;
+                        case 1:
+                            R = Vdec;
+                            G = V;
+                            B = Vmin;
+                            break;
+                        case 2:
+                            R = Vmin;
+                            G = V;
+                            B = Vinc;
+                            break;
+                        case 3:
+                            R = Vmin;
+                            G = Vdec;
+                            B = V;
+                            break;
+                        case 4:
+                            R = Vinc;
+                            G = Vmin;
+                            B = V;
+                            break;
+                        case 5:
+                            R = V;
+                            G = Vmin;
+                            B = Vdec;
+                            break;
+                    }
+                    image.SetPixel(i, j, Color.FromArgb((int)(R * 255/100), (int)(G * 255 / 100), (int)(B * 255 / 100)));
+                    maxRGB = -1;
+                    minRGB = 255;
                 }
             
             pictureBox1.Image = image;
             img = image;
-            DrawImage(img);
+            //DrawImage(img);
         }
 
         private void button1_Click(object sender, EventArgs e)
